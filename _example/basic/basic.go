217cab3715186181210f324d769b94c0118f7abc
xueqing tu <flyingyizi@gmail.com>
2018-12-26 21:56:43 +0800

    1

--- _example/basic/basic.go
+++ _example/basic/basic.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"bytes"
+	"encoding/base64"
+	"encoding/json"
 	"fmt"
+	"strings"
 
+	"github.com/PuerkitoBio/goquery"
 	"github.com/flyingyizi/spider"
-	"github.com/flyingyizi/spider/core"
 	"github.com/flyingyizi/spider/query"
 )
 
@@ -12,40 +16,96 @@ import (
 // https://m.uczzd.cn/webview/news?app=huawei-iflow&aid=16119487478051582585&cid=179223212&zzd_from=huawei-iflow&uc_param_str=dndsfrvesvntnwpfgibicpkt&recoid=7511051609959712598&rd_type=reco&sp_gz=1&enuid=AANA1zeV8aROM%2FjV3SPP4HAxwsLLuvuIpl56ol5ZzlXQuw%3D%3D
 
 func main() {
-	startURL := "https://m.uczzd.cn/webview/news?app=huawei-iflow&aid=3091260259678083253&cid=100&zzd_from=huawei-iflow&uc_param_str=dndsfrvesvntnwpfgibicpkt&recoid=859544137256752847&rd_type=reco&sp_gz=1&enuid=AANA1zeV8aROM%2FjV3SPP4HAxwsLLuvuIpl56ol5ZzlXQuw%3D%3D"
+	//startURL := "https://m.uczzd.cn/webview/news?app=huawei-iflow&aid=3091260259678083253&cid=100&zzd_from=huawei-iflow&uc_param_str=dndsfrvesvntnwpfgibicpkt&recoid=859544137256752847&rd_type=reco&sp_gz=1&enuid=AANA1zeV8aROM%2FjV3SPP4HAxwsLLuvuIpl56ol5ZzlXQuw%3D%3D"
+	startURL2 := "https://m.uczzd.cn/webview/news?app=huawei-iflow&aid=16119487478051582585&cid=179223212&zzd_from=huawei-iflow&uc_param_str=dndsfrvesvntnwpfgibicpkt&recoid=7511051609959712598&rd_type=reco&sp_gz=1&enuid=AANA1zeV8aROM%2FjV3SPP4HAxwsLLuvuIpl56ol5ZzlXQuw%3D%3D"
+	getUC(startURL2)
+}
+
+func getUC(url string) {
+	d := spider.DocCard{}
+
+	var bodyData []byte
 	// Instantiate default collector
-	c := spider.NewCollector(
-	// Visit only domains: hackerspaces.org, wiki.hackerspaces.org
-	//spider.AllowedDomains("hackerspaces.org", "wiki.hackerspaces.org"),
-	)
-
-	//*[@id="wrapper"]/div[13]
-	// On every a element which has href attribute call callback
-	c.OnHTML(`div[data-region="article_relate"]`, func(e *query.HTMLElement) {
-		fmt.Println(e.Text)
-		// link := e.Attr("href")
-		// // Print link
-		// fmt.Printf("Link found: %q -> %s\n", e.Text, link)
-		// // Visit link found on page
-		// // Only those links are visited which are in AllowedDomains
-		// c.Visit(e.Request.AbsoluteURL(link))
-	})
-
-	// Before making a request print "Visiting ..."
-	c.OnRequest(func(r *core.Request) {
-		fmt.Println("Visiting", r.URL.String())
-	})
+	c := spider.NewCollector()
 
 	c.OnResponse(func(r *spider.Response) {
-
-		d := spider.DocCard{}
-		d.Title = string(r.Body)
-
-		d.Save("abc.json")
-
-		//fmt.Println(string())
+		bodyData = r.Body
 	})
 
-	// Start scraping on https://hackerspaces.org
-	c.Visit(startURL)
+	//xpath: `//head/title`
+	c.OnHTML(`head>title`, func(e *query.HTMLElement) {
+		d.Title = strings.TrimSpace(e.Text)
+		//fmt.Println(d.Title)
+	})
+
+	err := c.Visit(url)
+	if err != nil {
+		return
+	}
+
+	htmlDoc, err := goquery.NewDocumentFromReader(bytes.NewReader(bodyData))
+	if err != nil {
+		return
+	}
+
+	scripts := htmlDoc.Find("script")
+
+	destScript := ""
+	//通过观察uc的网页，内容存放在js代码中，下面从js代码中将感兴趣的内容解析出来
+	key1, key2 := "xissJsonData", "content"
+	for _, n := range scripts.Nodes {
+		if n.FirstChild != nil {
+			//找了两个存放内容js代码的特征
+			if strings.Contains(n.FirstChild.Data, key1) && strings.Contains(n.FirstChild.Data, key2) {
+				destScript = n.FirstChild.Data
+				break
+			}
+		}
+	}
+	//fmt.Println(destScript)
+
+	//解析出js代码中xissJsonData变量的值
+	startIndex := strings.Index(destScript, key1)
+	xissJsonData := getContentInBrace(destScript, startIndex)
+	//fmt.Println(string(xissJsonData))
+
+	type XissJSONData struct {
+		Content string `json:"content"`
+	}
+	if err := json.Unmarshal(xissJsonData, &d); err == nil {
+		//fmt.Println(d)
+	} else {
+		fmt.Println(err)
+		return
+	}
+	d.OrigHTML = base64.StdEncoding.EncodeToString(bodyData)
+
+	d.Save("efg.json")
+
+}
+
+//抽取大括号中的内容,含定界的`{}`
+func getContentInBrace(str string, startIndex int) []byte {
+	buf := bytes.NewBufferString("")
+	var count int = 0
+	for i := startIndex; i < len(str); i++ {
+		if str[i] == '{' {
+			count++
+		}
+		if count != 0 {
+			buf.WriteByte(str[i])
+		}
+		if str[i] == '}' {
+			count--
+			if count == 0 {
+				break
+			}
+		}
+	}
+	if count != 0 {
+		return nil
+	}
+
+	// return buf.String()[1 : len(buf.String())-1]
+	return buf.Bytes() //   .String()
 }
--- tofile.go
+++ tofile.go
@@ -5,11 +5,37 @@ import (
 	"os"
 )
 
+// "content":"<p>自。</p><p>1、</p><p>舰。</p><!--{img: 0}--><p>2、</p><!--{img: 1 }--><div id=\"hidtag\"></div><p>3、</p><!--{img: 2}--><p>4、</p><p>！</p><!--{img: 3}--><p>5、</p>",
+// "thumbnails":[
+//     {"url":"http://image.uczzd.cn/2563535540031726725.jpg?id=0&from=export","width":640,"height":360,"type":"jpg","preload":0,"daoliu_url":"","daoliu_title":""},
+//     {"url":"http://image.uczzd.cn/11790983737676000867.jpg?id=0&from=export","width":640,"height":360,"type":"jpg","preload":0,"daoliu_url":"","daoliu_title":""},
+//     {"url":"http://image.uczzd.cn/18224486047257093302.jpg?id=0&from=export","width":615,"height":346,"type":"jpg","preload":0,"daoliu_url":"","daoliu_title":""}],
+// "images":[
+//     {"title":"","url":"http://image.uczzd.cn/14043054703237486508.jpg?id=0&from=export","description":"","index":0,"width":640,"height":360,"preload":1,"type":"jpg","focus":"50_50","original_url":"http://image.uc.cn/s/wemedia/s/upload/2018/b39193abd84b820fb3af1e732581cd5e.jpg","daoliu_url":"","daoliu_title":"","gallery_id":"","gallery_type":0,"is_hd":0,"image_extra":{}},
+//     {"title":"","url":"http://image.uczzd.cn/17372319831213558599.jpg?id=0&from=export","description":"","index":1,"width":640,"height":426,"preload":0,"type":"jpg","focus":"50_50","original_url":"","daoliu_url":"","daoliu_title":"","gallery_id":"","gallery_type":0,"is_hd":0,"image_extra":{}},
+//     {"title":"","url":"http://image.uczzd.cn/1479968059362464066.jpg?id=0&from=export","description":"","index":2,"width":640,"height":394,"preload":0,"type":"jpg","focus":"50_50","original_url":"","daoliu_url":"","daoliu_title":"","gallery_id":"","gallery_type":0,"is_hd":0,"image_extra":{}},
+//     {"title":"","url":"http://image.uczzd.cn/6819343685372118035.jpg?id=0&from=export","description":"","index":3,"width":640,"height":346,"preload":0,"type":"jpg","focus":"50_50","original_url":"","daoliu_url":"","daoliu_title":"","gallery_id":"","gallery_type":0,"is_hd":0,"image_extra":{}}],
+type Thumbnail struct {
+	Url    string `json:"url"`
+	Width  int    `json:"width"`
+	Height int    `json:"height"`
+	Type   string `json:"type"`
+}
+type Image struct {
+	Url    string `json:"url"`
+	Width  int    `json:"width"`
+	Height int    `json:"height"`
+	Type   string `json:"type"`
+	Index  int    `json:"index"`
+}
+
 //DocCard 存放内容的卡片
 type DocCard struct {
-	OrigHtml []byte
-	Content  []byte
-	Title    string
+	OrigHTML   string      `json:"origHtml"`
+	Content    string      `json:"content"`
+	Title      string      `json:"title"` //,omitempty在序列化的时候忽略0值或者空值
+	Thumbnails []Thumbnail `json:"thumbnails"`
+	Images     []Image     `json:"images"`
 }
 
 // Save saves model in the file
